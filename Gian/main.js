9f3e3aa50fe55d4851a2fb8226e56de143452e35
rkshankar05 <168268507+rkshankar05@users.noreply.github.com>
2024-06-05 11:04:06 +0530

    Add files via upload

---
+++ Gian/main.js
@@ -0,0 +1,42 @@
+let c= document.getElementsByClassName('box');
+let d=c.length;
+let f = document.getElementsByClassName('forward');
+let b = document.getElementsByClassName('backward');
+let index=[];
+function push_Zindex (){
+    for(let i=0;i<d;i++)
+        {
+            index.push(20-i);
+            c[i].style.zIndex=20-i;
+        }
+}
+push_Zindex();
+function forward()
+{   
+    let x = index[d-1];
+    for(let i=d-1;i>0;i--)
+        {
+            index[i]=index[i-1];
+        }
+        index[0]=x; 
+     for(let i=0;i<=d-1;i++)
+        {
+            c[i].style.zIndex=index[i];
+        }
+};
+f[0].addEventListener('click', forward);
+function backward()
+{
+    let x = index[0];
+    for(let i=0;i<d-1;i++)
+        {
+            index[i]=index[i+1];
+        }
+        index[d-1]=x;
+    for(let i=0;i<d;i++)
+        {
+            c[i].style.zIndex=index[i];
+        }
+
+}
+b[0].addEventListener('click', backward);
